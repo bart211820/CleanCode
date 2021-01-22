d22e93b76581a650467a5bb326a23fb5932b29f2
Bart <bartlans76@gmail.com>
2021-01-22 21:14:26 +0100

    Removed default email and password
    This was only done to make it easier to login without having to type in the data everytime

--- webshop/src/app/webshop/regist-in/login/login.component.ts
+++ webshop/src/app/webshop/regist-in/login/login.component.ts
@@ -24,8 +24,8 @@ export class LoginComponent implements OnInit {
   constructor(private api: ApiService, private authService: AuthorizationService, private router: Router, private userService: UserService) { }
 
   ngOnInit() {
-    this.email = 'second@user.com';
-    this.password = 'second';
+    this.email = '';
+    this.password = '';
   }
 
   login() {
